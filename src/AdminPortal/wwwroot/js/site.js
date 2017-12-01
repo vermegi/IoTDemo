56265a48edb3ead2b1a5c86b3517dc9a7c210381
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-12-01 13:54:11 +0000

    extra javascript

--- src/AdminPortal/wwwroot/js/site.js
+++ src/AdminPortal/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 // Write your Javascript code.
+
+var app = angular.module('IotDemo', ['ui.bootstrap']);
+
+app.run(function () { });
+
+app.controller('DeviceEmulatorController', ['$rootScope', '$scope', '$http', '$timeout', function ($rootScope, $scope, $http, $timeout) {
+
+    $scope.refresh = function () {
+        
+    };
+
+}]);
