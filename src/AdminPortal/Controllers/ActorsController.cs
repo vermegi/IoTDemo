7b3b163bacd18372fd8d4699e059f3a5df202e33
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-12-13 07:47:04 +0000

    trying to get the number of actors

---
+++ src/AdminPortal/Controllers/ActorsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Actors.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdminPortal.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ActorsController : Controller
+    {
+        public ActorsController() { }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var activeActors = new List<ActorInformation>();
+
+            for (var i = 0; i < 2; i++) //TODO: change upper limit based on query to service fabric manager
+            {
+                var actorServiceProxy = ActorServiceProxy.Create(new Uri("fabric:/IotDemoApp/IoTDeviceActorService"), i);
+
+                ContinuationToken continuationToken = null;
+                var cancellationToken = new CancellationToken();
+
+                do
+                {
+                    var page = await actorServiceProxy.GetActorsAsync(continuationToken, cancellationToken);
+                    activeActors.AddRange(page.Items);
+                    continuationToken = page.ContinuationToken;
+                }
+                while (continuationToken != null);
+            }
+
+            return Json(activeActors.Count);
+        }
+
+    }
+}
